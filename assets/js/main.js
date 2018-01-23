ed8c7108b8f3b5f1d8d1b7dc23b07d5189b35953
Darrel Nato <johndarrelnato@gmail.com>
2018-01-23 14:52:50 +0800

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	$(".burger").on("click",function(e){
+		$( this ).toggleClass( "cross" );
+		$( '#hidden-menu' ).toggleClass( "show" );
+    });
+
+    $(".hm-link").click(function() {
+    	$( ".burger" ).removeClass( "cross" );
+    	$( '#hidden-menu' ).removeClass( "show" );
+    });
+
+  $('.tilt-js').tilt({
+    perspective: 10000
+  })
+});
