4555dbd045ed0dbf70f53f0cfc36d8f32a1fcf1a
Darrel Nato <johndarrelnato@gmail.com>
2020-03-15 14:32:17 +0800

    placeholder release

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,44 @@
-$(document).ready(function() {
+var cursor = $(".custom-cursor"),
+  follower = $(".cursor-trail");
 
-	$('.hamburger').click(function() {
-		$(this).toggleClass('opened');
-	});
+var posX = 0,
+  posY = 0;
+
+var mouseX = 0,
+  mouseY = 0;
+
+TweenMax.to({}, 0.016, {
+  repeat: -1,
+  onRepeat: function() {
+    posX += (mouseX - posX) / 5;
+    posY += (mouseY - posY) / 5;
+
+    TweenMax.set(follower, {
+      css: {    
+        left: posX - 17.5,
+        top: posY - 17.5
+      }
+    });
+
+    TweenMax.set(cursor, {
+      css: {    
+        left: mouseX,
+        top: mouseY
+      }
+    });
+  }
+});
+
+$(document).on("mousemove", function(e) {
+  mouseX = e.pageX;
+  mouseY = e.pageY;
+});
+
+$("a").on("mouseenter", function() {
+  cursor.addClass("active");
+  follower.addClass("active");
+});
+$("a").on("mouseleave", function() {
+  cursor.removeClass("active");
+  follower.removeClass("active");
 });
